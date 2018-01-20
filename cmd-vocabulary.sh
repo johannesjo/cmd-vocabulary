12c6dd7491a1bc2dba43349ecefcb45a2b219c66
Johannes Millan <johannes.millan@gmail.com>
2018-01-20 13:42:25 +0100

    refine whole functionality; add multiple lines, and additional sentences support

--- cmd-vocabulary.sh
+++ cmd-vocabulary.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 # echo "The script you are running has basename `basename $0`, dirname `dirname $0`"
 # echo "The present working directory is `pwd`"
-file=${1:-`dirname $0`/default-vocabulary.yml}
-line=`shuf -n1 $file`
-printf "$line\n"
+
+STR=''
+NL='
+'
+LPURPLE='\033[01;35m'
+YELLOW='\033[00;33m'
+RESTORE='\033[0m'
+
+
+FILE=${1:-`dirname $0`/default-vocabulary.yml}
+LINE=`shuf -n1 $FILE`
+
+# split word and translation
+IFS=':' read -ra ARRAY <<< "$LINE"
+STR="${LPURPLE}${ARRAY[0]}:${RESTORE}"
+
+IFS=';' read -ra ARRAY2 <<< "${ARRAY[1]}"
+
+STR="$STR${YELLOW}${ARRAY2[0]}${RESTORE}"
+for entry in "${ARRAY2[@]:1}"; do
+     STR="$STR ${NL} $entry"
+done
+
+
+
+
+echo -e "${STR}"
